2fa53485b0d9c3efc3979a844bf5bf96f784b212
charlieroth <charlieroth4@gmail.com>
2024-12-22 20:24:51 +0100

    health check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{http::StatusCode, routing::get, Router};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let router = Router::new().route("/health", get(health_handler));
+    let listener = TcpListener::bind("0.0.0.0:8000").await.unwrap();
+    axum::serve(listener, router).await.unwrap();
+    Ok(())
+}
+
+async fn health_handler() -> (StatusCode, &'static str) {
+    (StatusCode::OK, "OK")
 }
